ce25bb0860d568bc6a1a5ac4b21bc9a5e458abc2
achille56 <leclercqjm59@gmail.com>
2022-11-19 21:06:33 +0100

    exo 51

---
+++ style.js
@@ -0,0 +1,29 @@
+let nB1 = 5;
+let nB2 = 6;
+
+let result = nB1 + nB2;
+console.log(result);
+
+let addition;
+addition = result + 2;
+console.log(addition);
+
+let soustraction;
+soustraction = result - 1;
+console.log(soustraction);
+
+alert(result * addition);
+
+
+alert(result ** soustraction);
+
+
+let m1 = 5;
+let m2 = 2;
+let modulo = m1 % m2;
+console.log(modulo);
+
+
+
+
+
